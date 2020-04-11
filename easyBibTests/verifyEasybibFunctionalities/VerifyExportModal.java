1d455cd8441e19352d09c0bf566525c90e67a277
Jeel Patel <jeel.patel@verizonwireless.com>
2020-04-11 18:08:34 -0400

    Verify opening of Export modal test code

---
+++ easyBibTests/verifyEasybibFunctionalities/VerifyExportModal.java
@@ -0,0 +1,44 @@
+package verifyEasybibFunctionalities;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import initialSettings.BrowserSetting;
+import objectRepository.CitationPage;
+
+/**
+ * This class tests the opening of Export modal
+ * @author Jeel Patel
+ * @version 1.0
+ *
+ */
+public class VerifyExportModal {
+	BrowserSetting brSet;
+	WebDriver driver;
+
+	@BeforeClass
+	public void driverInitialization(){
+
+		brSet = new BrowserSetting();
+		driver = brSet.BrowserSettings();
+	}
+	@Test
+	public void verifyExportModal() {
+		
+		CitationPage cp = new CitationPage(driver);
+		
+		// Clicking on Export button
+		cp.exportButtonElement().click();
+		
+		// Verify "Export Modal" opened after clicking the "Export" button
+		Assert.assertTrue(cp.exportModalElement().isDisplayed());
+		}
+	
+	@AfterClass
+	public void driverClosure() {
+		driver.close();
+	}
+}
